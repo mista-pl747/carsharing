05e8426bf82bc46f5253edfa8543032ba6eb726d
mista-pl747 <daniilgorn2704@gmail.com>
2025-05-22 09:20:47 +0200

    Add files via upload

---
+++ carsharing/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'carsharing_db',
+        'USER': 'root',
+        'PASSWORD': 'ukr436504',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+} 
